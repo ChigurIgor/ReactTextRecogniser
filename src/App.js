4db82e1cbb8ae7b51f7cca8b5fa12f1cc8f5c9ab
ChigurIgor <chigur.ig@gmail.com>
2022-02-09 08:51:01 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import Webcam from "react-webcam";
+import React, {useRef, useState, useCallback, createRef, useEffect} from 'react';
+import { createWorker } from 'tesseract.js';
 
 function App() {
+  const webcamRef = useRef(null);
+  const [imgSrc, setImgSrc] = useState();
+  const [text, setText] = useState("");
+    const worker = createWorker({
+        logger: m => console.log(m)
+    });
+    const videoConstraints = {
+        // facingMode: "user"
+        facingMode: { exact: "environment" }
+
+    };
+    const capture = React.useCallback(
+      () => {
+        const imageSrc = webcamRef.current.getScreenshot();
+        setImgSrc(imageSrc);
+      },
+      [webcamRef]
+  );
+
+useEffect(()=> recognizeText(imgSrc), [imgSrc]);
+
+   const recognizeText = (imgSrc) =>{
+       if(imgSrc) {
+           (async () => {
+               await worker.load();
+               await worker.loadLanguage('eng');
+               await worker.initialize('eng');
+               const { data: { text } } = await worker.recognize(imgSrc);
+               console.log(text);
+               setText(text);
+               await worker.terminate();
+           })();
+       }
+    }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Webcam
+          videoConstraints={videoConstraints}
+          style={{height:'40vh'}}
+          audio={false}
+          // height={720}
+          ref={webcamRef}
+          // width={1280}
+          screenshotFormat="image/jpeg"
+      />
+      <button  onClick={capture}>
+        Capture
+      </button>
+      <img style={{height:'20vh'}} alt="captured" src={imgSrc}/>
+      <p>{text}</p>
     </div>
   );
 }
